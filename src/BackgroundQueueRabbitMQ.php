7d7957d93d4a564489f4c4d52b849fe482f5b074
Tomáš Kudělka <thorewi@gmail.com>
2023-05-10 16:47:39 +0200

    Update BackgroundQueueRabbitMQ.php

--- src/BackgroundQueueRabbitMQ.php
+++ src/BackgroundQueueRabbitMQ.php
@@ -13,8 +13,6 @@ class BackgroundQueueRabbitMQ
 {
 	const NOOP = 'noop';
 
-	const MIN_EXECUTION_TIME = 1;
-
 	const PRODUCER_GENERAL = 'general';
 
 	private Connection $connection;
@@ -46,8 +44,6 @@ class BackgroundQueueRabbitMQ
 	 */
 	public function process(AMQPMessage $message): bool
 	{
-		$executionTime = -microtime(true);
-
 		$body = $message->getBody();
 
 		if ($body === self::NOOP) {
@@ -55,16 +51,6 @@ class BackgroundQueueRabbitMQ
 		}
 
 		$this->backgroundQueue->process((int) $body);
-
-		/**
-		 * Jedno zpracování je případně uměle protaženo sleepem, aby si *supervisord*
-		 * nemyslel, že se proces ukončil moc rychle.
-		 */
-		$executionTime += microtime(true);
-		if ($executionTime < self::MIN_EXECUTION_TIME) {
-			// Pokud bychom zpracovali řádek z fronty moc rychle, udělej sleep
-			usleep((int) ((self::MIN_EXECUTION_TIME - $executionTime) * 1000 * 1000));
-		}
 
 		// vždy označit zprávu jako provedenou (smazat ji z rabbit DB)
 		return true;
