e7f88f3cb380d7b70f44c255f237251563cc57b3
Tomáš Kudělka <thorewi@gmail.com>
2023-06-25 09:42:25 +0200

    Update BackgroundQueueRabbitMQ.php

--- src/BackgroundQueueRabbitMQ.php
+++ src/BackgroundQueueRabbitMQ.php
@@ -25,9 +25,9 @@ class BackgroundQueueRabbitMQ
 		$this->backgroundQueue = $backgroundQueue;
 	}
 
-	public function publish(int $id, ?string $producer = null): void
+	public function publish(int $id, ?string $producer = self::PRODUCER_GENERAL): void
 	{
-		$this->connection->getProducer($producer ?: self::PRODUCER_GENERAL)->publish($id);
+		$this->connection->getProducer($producer)->publish($id);
 	}
 
 	public function publishNoop(): void
